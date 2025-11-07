3b544322715d41fb77e559703f754eebc28012e9
uqlism <uqlizm@gmail.com>
2025-11-08 00:49:53 +0900

    キャラクターの整理と遊び方を追加

--- src/vordle.ts
+++ src/vordle.ts
@@ -23,118 +23,178 @@ const wordLen = 6
 const maxGuessCount = 6
 
 const answerWords = [
-    "adati", // 足立レイ
-    "ai",
-    "akane",
-    "akari",
-    "allial",
-    "amato",
-    "aoi",
-    "aoyama",
-    "asumi",
-    "fee", // FEEちゃん
-    "flower",
-    "futaba", // 双葉湊音
-    "galaco",
-    "gumi", // GUMI
-    "hatune", // 初音ミク
-    "hau", // 雨晴はう
-    "himari",
-    "hime", // 鳴花ヒメ
-    "ia",
-    "icalo",
-    "iori",
-    "itako",
-    "kafu",
-    "kaito",
-    "natuki", // 夏色花梨
-    "karin", // 夏色花梨 谷崎カリン
-    "koharu",
-    "kotone",
-    "kou",
-    "len",
-    "maki",
-    "meiko",
-    "meimei",
-    "metan",
-    "miko", // 櫻歌ミコ
-    "mikoto", // 鳴花ミコト
-    "miku",
-    "minase",
-    "minato", // 双葉湊音
-    "moca", // 宮舞モカ
-    "motiko", // もち子さん
-    "one", // OИE
-    "rei", // 足立レイ
-    "rikka",
-    "rime",
-    "rin",
-    "ririse",
-    "matuka", // 松樺りすく
-    "risuku", // 松樺りすく
-    "ritu", // 波音リツ
-    "riu",
-    "ryusei",
-    "sasara",
-    "satou",
-    "sayo",
-    "seika",
-    "sekai",
-    "shuo",
-    "sikoku",
-    "suzuki",
-    "teto",
-    "tobari",
-    "tohoku",
-    "tt", // ナースロボ_タイプT
-    "tudumi",
-    "tuina",
-    "tumugi",
-    "tunose",
-    "una",
-    "uni",
-    "unoka",
-    "urone",
-    "usagi",
-    "yuduki",
-    "yuduru",
-    "yukari",
-    "yuki",
-    "yukine",
-    "zunko",
-    "meika", // 鳴花ヒメ 鳴花ミコト
-    "tifuyu",
-    "kizuna",
-    "kasane", // 重音テト
-    "sora", // 桜乃そら | 九州そら
-    "haruno", // 桜乃そら
-    "kazami", // 風見壮一
-    "souiti", // 風見壮一
-    "tange", // タンゲコトエ
-    "kotoe", // タンゲコトエ
-    "kakyou", // カキョウヨサリ
-    "yosari", // カキョウヨサリ
-    "soyogi", // 梵そよぎ
-    "miki", // 初音ミク
-    "ui", // 雨衣
-    "rosa", // ROSA
+    // Special
+
+    ["icalo", "イカロ神"],
+    ["vomin", "ぼ民"],
+
+    // A.I.Voice
+
+    ["yuduki", "結月ゆかり"],
+    ["yukari", "結月ゆかり"],
+    ["kizuna", "紲星あかり"],
+    ["akari", "紲星あかり"],
+    ["akane", "琴葉茜"],
+    ["aoi", "琴葉葵"],
+    ["iori", "伊織弓弦"],
+    ["yuduru", "伊織弓弦"],
+
+    ["unoka", "羽ノ華"],
+    ["tange", "タンゲコトエ"],
+    ["kotoe", "タンゲコトエ"],
+    ["kakyou", "カキョウヨサリ"],
+    ["yosari", "カキョウヨサリ"],
+    ["tobari", "夜語トバリ"],
+    ["kotone", "紡乃世詞音"],
+    ["tunose", "紡乃世詞音"],
+    ["riu", "雪音りう"],
+    ["yukine", "雪音りう"],
+    ["kazami", "風見壮一"],
+    ["souiti", "風見壮一"],
+    ["soyogi", "梵そよぎ"],
+
+    ["meika", "鳴花ヒメ 鳴花ミコト"],
+    ["hime", "鳴花ヒメ"],
+    ["mikoto", "鳴花ミコト"],
+    ["flower", "v-flower c-flower"],
+    ["sinfa", "心華"],
+
+    // CEVIO
+
+    ["sasara", "さとうささら"],
+    ["satou", "さとうささら"],
+    ["suzuki", "すずきつづみ"],
+    ["tudumi", "すずきつづみ"],
+    ["amato", "タカハシアマト"],
+
+    ["natuki", "夏色花梨"],
+    ["karin", "夏色花梨 谷崎カリン"],
+    ["koharu", "小春六花"],
+    ["rikka", "小春六花"],
+    ["tifuyu", "花隈千冬"],
+
+    ["minato", "双葉湊音"],
+    ["futaba", "双葉湊音"],
+
+    ["asumi", "彩澄りりせ 彩澄しゅお"],
+    ["ririse", "彩澄りりせ"],
+    ["syuo", "彩澄しゅお"],
+
+    ["fee", "CCD-0500 FEE"],
+    ["uni", "CCD-0001 UNI"],
+
+    ["moka", "宮舞モカ"],
+    ["maki", "弦巻マキ"],
+
+    // VoiceVox
+
+    ["tohoku", "東北イタコ 東北ずんこ 東北きりたん"],
+    ["itako", "東北イタコ"],
+    ["zunko", "東北ずんこ"],
+    ["metan", "四国めたん"],
+    ["sikoku", "四国めたん"],
+    ["usagi", "中国うさぎ"],
+    ["sora", "九州そら 桜乃そら"],
+    ["kyusyu", "九州ソラ"],
+
+    ["haruno", "桜乃そら"],
+
+    ["himari", "冥鳴ひまり"],
+    ["tumugi", "春日部つむぎ"],
+    ["meimei", "冥鳴ひまり"],
+
+    ["sayo", "小夜"],
+    ["ouka", "櫻歌ミコ"],
+    ["miko", "櫻歌ミコ"],
+
+    ["ryusei", "青山龍星"],
+    ["aoyama", "青山龍星"],
+
+    ["motiko", "もち子さん"],
+    ["hau", "雨晴はう"],
+
+    // Lusty*Kiss_Production
+
+    ["kurowa", "黒朱乃宮・セプテントリオーネス・ラ・クロワ"],
+    ["ririn", "黒朱乃宮・ティンティナーブルム・リリン"],
+    ["dhia", "ディアマンテ・プリンシパリティーズ"],
+    ["aruma", "アルマジェダーニャ・プルガトリア"],
+    ["anzii", "アンドロマリウス・プルガトリア"],
+
+    // CoeiroInk
+
+    ["reko", "レコ"],
+    ["nako", "ナコ"],
+    ["kanata", "カナタ"],
+
+    ["matuka", "松樺りすく"],
+    ["risuku", "松樺りすく"],
+
+    // Song Softwares
+
+    ["hatune", "初音ミク"],
+    ["miku", "初音ミク"],
+    ["ren", "鏡音レン"],
+    ["rin", "鏡音リン"],
+    ["meiko", "MEIKO"],
+    ["kaito", "KAITO"],
+
+    ["gumi", "GUMI"],
+    ["gakupo", "神威がくぽ"],
+
+    ["one", "OИE"],
+    ["ia", "IA"],
+
+    ["kaai", "歌愛ユキ"],
+    ["yuki", "歌愛ユキ"],
+
+    ["kafu", "可不"],
+    ["sekai", "星界"],
+    ["rime", "裏命"],
+
+    ["ui", "雨衣"],
+    ["galaco", "ギャラ子"],
+    ["una", "音街ウナ"],
+
+    // Others
+
+    ["adati", "足立レイ"],
+    ["rei", "足立レイ"],
+
+    ["minase", "水瀬コウ"],
+    ["kou", "水瀬コウ"],
+
+    ["teto", "重音テト"],
+    ["kasane", "重音テト"],
+
+    ["namine", "波音リツ"],
+    ["ritu", "波音リツ"],
+    ["ariaru", "アリアル"],
+    ["seika", "京町せいか"],
+    ["tt", "ナースロボ_タイプT"],
+    ["tuina", "役ついな"],
+    ["urone", "虚音イフ"],
+    ["ifu", "虚音イフ"],
+
+    ["ai", "月読アイ"],
+    ["miki", "開発コード-MIKI"],
+    ["rosa", "ROSA"],
 ]
+
 const guessableWords = [
-    "aichan",
+    "kamui", // 神威がくぽ
+    "aichan", // 月読アイ
     "aisuu", // AiSuu
-    "akashi",
     "akasi",
-    "ar", // 猫使アル
-    "arma",
+    "aru", // 猫使アル
     "asahi",
     "ashie",
     "avanna",
     "awase",
     "ayane",
-    "b", // 猫使ビィ
-    "chisa", // 知声
+    "bii", // 猫使ビィ
+    "chisei", // 知声
     "cotumu",
-    "croix", // クロワ
     "dayone",
     "denri",
     "frimo",
@@ -142,18 +202,15 @@ const guessableWords = [
     "goki", // 後鬼
     "haru", // 羽累
     "hxvoc", // HXVOC [SV]
-    "if", // 虚音イフ
     "ion",
-    "kanata", // カナタ [COEIROINK]
     "kei",
     "kemuri",
     "koko", // 狐子
     "komako",
     "kon",
-    "leur", // ルウル
+    "ruuru", // ルウル
     "likea", // 来果
     "raika", // 来果
-    "lilin", // リリンちゃん
     "mana",
     "maron", // 栗田まろん
     "masiro",
@@ -165,15 +222,12 @@ const guessableWords = [
     "mon",
     "muon",
     "mutsu",
-    "nako",
     "nana", // マクネナナ
     "nekoi",
     "nigari",
     "noise", // FEE NOiSE
     "omito",
-    "ouka", // 櫻歌ミコ
     "poron", // ポロンちゃん [VOICEPEAK]
-    "reco",
     "rino",
     "rito",
     "sakuya",
@@ -198,26 +252,21 @@ const guessableWords = [
     "meron", // 北海道めろん
     "uduki", // 黒聡鵜月
     "uta",
-    "vomin",
     "yixi",
     "yuna",
     "yojika",
     "yosi",
     "yutaro",
-    "namine", // 波音リツ
     "makune", // マクネナナ
-    "carune",
     "kuzuda",
     "yone",
     "rou",
     "nia",
-    "dia",
     "vyone",
     "vytwo",
     "typet", // ナースロボ_タイプT
     "kukuri", // 八蜂鞠ククリ
     "koron", // 涙目コロン
-    "kyusyu", // 九州ソラ
     "sahra", // サーラちゃん様
     "ria", // RIA
     "wakaho", // 若穂みのり
@@ -230,8 +279,7 @@ const guessableWords = [
     "yuu", //zora
     "wil", //zora
     "unity", // Unity-chan
-    "xinhua", // 心華
-    "enno", // 役ついな
+    "ennno", // 役ついな
     "kurono", // VirVox
     "genbu", // VirVox
     "kotaro", // VirVox
@@ -292,6 +340,8 @@ const guessableWords = [
     "luo", // 洛天依
     "tianyi", // 洛天依
     "yosida", // 吉田君
+    "namae", // 名前シレズ
+    "sirezu", // 名前シレズ
 ]
 
 const compare = (input: string[], answer: string[]): Guess => {
@@ -322,7 +372,8 @@ const playTimes: number = record === undefined ? 1 : record[today] === undefined
 
 function play() {
     const rnd = Math.gen_rng(`${today}/${playTimes}`)
-    const answer = answerWords[rnd(0, answerWords.len - 1)].pad_end(wordLen)
+    const [_answer, answerCharacterName] = answerWords[rnd(0, answerWords.len - 1)]
+    const answer = _answer.pad_end(wordLen)
     const ansLetters = answer.to_arr()
 
     const guessIdx = kiwi.state(0)
@@ -387,7 +438,7 @@ function play() {
         const guessWord = _guesses[_guessIdx]
 
         const guessStr = guessWord.map(x => x.letter).join("").trim()
-        if (!answerWords.incl(guessStr) && !guessableWords.incl(guessStr)) {
+        if (!answerWords.map(x => x[0]).incl(guessStr) && !guessableWords.incl(guessStr)) {
             errMsg.set(`未登録の単語です`)
             return
         }
@@ -435,37 +486,57 @@ function play() {
         return letters.map(({ color, char }) => `$[bg.color=${colorMap[color].bg} $[fg.color=${colorMap[color].fg}  ${char.upper()} ]]`).join(" ")
     }
 
-    Ui.render(serialArr([
-        () => Ui.C.text({ text: `Vordle ${today}-${playTimes}`, }),
-        () => kiwi.mfm({ text: () => render(guesses.get(), win.get()) }),
-        () => kiwi.container({
-            hidden: finished.get,
-            children: serialArr([
-                () => kiwi.mfm({ text: displayChars }),
-                () => kiwi.textInput({ default: inputBox.get, onInput: trySetGuessWord }),
-                () => kiwi.text({ text: errMsg.get }),
-                () => kiwi.buttons({
-                    buttons: () => [
-                        { disabled: errMsg.get() !== "", text: "入力", onClick: guess },
-                        { text: big.get() ? "縮小表示" : "拡大表示", onClick: () => big.set(!big.get()) },
-                        { text: "正解を見る", onClick: () => onFinished() }
-                    ]
-                }
-                )
-            ])
-        }),
-        () => kiwi.container({
-            hidden: () => !finished.get(),
-            align: "center",
-            children: [
-                kiwi.container({
-                    hidden: win.get,
-                    children: [kiwi.text({ text: `正解: ${answer}` })]
-                }),
-                kiwi.postFormButton({ primary: true, rounded: true, text: "結果を投稿", form: () => ({ text: postText() }) })
-            ]
-        })
-    ]))
+    const showHowToPlay = kiwi.state(false)
+    const howToPlay = [
+        "wordleを元にした合成音声キャラをあてるゲームです",
+        "答えのキャラクターを予想して6文字以下のローマ字で入力ましょう",
+        `答えに含まれる文字は $[bg.color=${colorMap[Color.hit].bg}  黄色 ]に`,
+        `文字の位置も一致していたら$[bg.color=${colorMap[Color.same].bg}  緑 ]に`,
+        `答えに含まれない文字は$[bg.color=${colorMap[Color.miss].bg}  灰色 ]になります`,
+        "6回以内にあてることができればクリアです",
+        "答えのキャラクターは必ず6文字以下です",
+    ].join(Str.lf)
+    Ui.render(
+        serialArr([
+            () => kiwi.container({
+                hidden: () => !showHowToPlay.get(),
+                children: serialArr([
+                    () => kiwi.mfm({ text: howToPlay }),
+                    () => kiwi.button({ text: "ゲームに戻る", onClick: () => showHowToPlay.set(false) }),
+                ])
+            }),
+            () => kiwi.container({
+                hidden: () => showHowToPlay.get(),
+                children: serialArr([
+                    () => Ui.C.text({ text: `Vordle ${today}-${playTimes}`, }),
+                    () => kiwi.mfm({ text: () => render(guesses.get(), win.get()) }),
+                    () => kiwi.container({
+                        hidden: finished.get,
+                        children: serialArr([
+                            () => kiwi.mfm({ text: displayChars }),
+                            () => kiwi.textInput({ default: inputBox.get, onInput: trySetGuessWord }),
+                            () => kiwi.text({ text: errMsg.get }),
+                            () => kiwi.buttons({
+                                buttons: () => [
+                                    { disabled: errMsg.get() !== "", text: "入力", onClick: guess },
+                                    { text: big.get() ? "縮小表示" : "拡大表示", onClick: () => big.set(!big.get()) },
+                                    { text: "遊び方", onClick: () => showHowToPlay.set(true) },
+                                ]
+                            }
+                            )
+                        ])
+                    }),
+                    () => kiwi.container({
+                        hidden: () => !finished.get(),
+                        align: "center",
+                        children: serialArr([
+                            () => kiwi.text({ text: `正解: ${answer} | ${answerCharacterName}` }),
+                            () => kiwi.postFormButton({ primary: true, rounded: true, text: "結果を投稿", form: () => ({ text: postText() }) })
+                        ])
+                    })
+                ])
+            })
+        ]))
 }
 
 play()
